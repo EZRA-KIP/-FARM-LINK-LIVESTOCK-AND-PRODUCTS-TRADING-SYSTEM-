b5756d61353f419ba43810ce8e33469bbb8a68f7
Ezra <ezrasmaei65@gmail.com>
2025-07-23 15:53:45 +0300

    Restructure Django app for standard deployment

--- urls.py
+++ urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('trading.urls')),
-    # React SPA fallback for all non-API, non-admin, non-static/media routes
-    re_path(
-        r'^(?!api/|admin/|static/|media/).*$', 
-        TemplateView.as_view(template_name='index.html')
-    ),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
